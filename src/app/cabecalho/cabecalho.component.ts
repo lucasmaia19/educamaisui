848a3607a1bdb71886dc94cbed20d2ff92071046
lucasmaia19 <lucasmaiaalves19@gmail.com>
2020-12-11 19:21:04 -0300

    comecado conversao

--- src/app/cabecalho/cabecalho.component.ts
+++ src/app/cabecalho/cabecalho.component.ts
@@ -2,11 +2,12 @@ import { HttpClient } from '@angular/common/http';
 import { MessageService } from 'primeng/api';
 import { FileUpload } from 'primeng/fileupload';
 import { Component, OnInit, ViewChild } from '@angular/core';
+import * as moment from 'moment';
 
 export class Cabecalho {
     professora?: string;
     turma?: string;
-    data?: string;
+    data?: Date;
     aluno?: string;
     nomeEscola?: string;
     logradouro?: string;
@@ -44,12 +45,12 @@ export class CabecalhoComponent implements OnInit {
 
     this.cabecalho = { professora: 'Flavia',
                         aluno: 'Lucas',
-                        data: '19/03/2021',
                         turma: 'Turma B',
                         nomeEscola: 'IFTM',
                         logradouro: 'Fazenda capim branco',
                         tel: '32262292',
                         cep: '38401591',
+                        data: new Date(),
                         email: 'lucasmaia@gmail.com',
                     };
 
@@ -65,41 +66,61 @@ export class CabecalhoComponent implements OnInit {
         clear: 'Limpar'
         }
 
-  }
-
-  uploadComDadosCabecalho(): void {
-
-    if (this.requestProgress) {
-        return;
-      }
-
-    this.requestProgress = true;
-
-    const formData = new FormData();
-
-    const dados = this.cabecalho;
-    Object.keys(dados).forEach(k => {
-        formData.append(k, dados[k]);
-    });
-
-    const logoPrefeitura = this.logoPrefeitura._files[0];
-    if (logoPrefeitura !== undefined) {
-        formData.append('logoPrefeitura', logoPrefeitura);
     }
 
-    const logoEscola = this.logoEscola._files[0];
-    if (logoEscola !== undefined) {
-        formData.append('logoEscola', logoEscola);
-    }
+    uploadComDadosCabecalho(): void {
 
-    this.http.post(this.apiuploadComDadosUrl, formData)
+        console.log(this.cabecalho.data)
+
+        let data = moment(this.cabecalho.data, "DDMMYYYY");
+        console.log(data);
+
+        const dataFormatada = data.format("YYYY-MM-DD");
+        console.log(dataFormatada);
+
+        this.cabecalho.data = data.toDate();
+
+        // console.info("data tipo Moment " + data.format("DD/MM/YYYY"));
+        // data.format("DD/MM/YYYY")
+
+        // return;
+
+
+
+
+        console.log(this.cabecalho.data)
+
+        if (this.requestProgress) {
+            return;
+        }
+
+        this.requestProgress = true;
+
+        const formData = new FormData();
+
+        const dados = this.cabecalho;
+        Object.keys(dados).forEach(k => {
+            formData.append(k, dados[k]);
+        });
+
+        const logoPrefeitura = this.logoPrefeitura._files[0];
+        if (logoPrefeitura !== undefined) {
+            formData.append('logoPrefeitura', logoPrefeitura);
+        }
+
+        const logoEscola = this.logoEscola._files[0];
+        if (logoEscola !== undefined) {
+            formData.append('logoEscola', logoEscola);
+        }
+
+        this.http.post(this.apiuploadComDadosUrl, formData)
         .toPromise()
-        .then(response => {
-        this.messageService.add({severity:'success', summary:'Cadastro adicionado com sucesso!'});
-        console.log("dados:", this.cabecalho)
+        .then(cabecalho => {
+            console.log(cabecalho)
+            this.messageService.add({severity:'success', summary:'Cadastro adicionado com sucesso!'});
+        })
 
         this.requestProgress = false;
-    })
-}
+    }
 
 }
