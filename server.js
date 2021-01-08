e7b6681de5803f6c25b85e1721a26e0b932d685f
lucasmaia19 <lucasmaiaalves19@gmail.com>
2021-01-08 17:42:45 -0300

    preparando para deploy

--- server.js
+++ server.js
@@ -1,14 +1,10 @@
 const express = require('express');
 const app = express();
-const path = require('path');
 
 app.use(express.static(__dirname + '/dist'));
-app.listen(process.env.PORT || 8080);
 
-//PATH LOCATION STARTEGY
+app.get('/*', function(req, res) {
+  res.sendFile(__dirname + '/dist/index.html');
+});
 
-app.get('/*', function(req,res){
-  const fullPath = path.join(__dirname + '/dist/index.html');
-  console.log(" Fetching from.." + fullPath);
-    res.sendFile(fullPath);
-})
+app.listen(process.env.PORT || 4200);
