95a0fbf6330a01f36040f2b0f54a2e0e6420cab8
lucasmaia19 <lucasmaiaalves19@gmail.com>
2021-01-08 21:20:32 -0300

    preparando para deploy

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
+app.use(express.static(__dirname + '/dist/educa-facil-ui'));
 
-app.use(express.static(__dirname + '/dist'));
 
 app.get('/*', function(req, res) {
   res.sendFile(__dirname + '/dist/index.html');
